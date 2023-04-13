a4df78a0fa75f4a68c2af6f566d64e8a9154e277
Jakub Knapik <knapikj.priv@gmail.com>
2023-04-13 12:47:36 +0200

    Skrypt zmodyfikowany o --help

--- skrypt.sh
+++ skrypt.sh
@@ -8,7 +8,12 @@ else
 fi
 
 if [[ $1 == "--logs" ]]; then
-  for i in {1..100}
+  if [[ $2 =~ ^[0-9]+$ ]]; then
+    num_logs=$2
+  else
+    num_logs=100
+  fi
+  for i in $(seq 1 $num_logs)
   do
     filename="log${i}.txt"
     echo "Nazwa pliku: ${filename}" > ${filename}
@@ -16,5 +21,15 @@ if [[ $1 == "--logs" ]]; then
     echo "Data: $(date)" >> ${filename}
   done
 else
-  echo "Nieprawidłowa flaga. Użyj --logs, aby utworzyć pliki logów."
+  echo "Nieprawidłowa flaga lub brak argumentu liczby plików. Użyj --logs [liczba], aby utworzyć określoną liczbę plików logów lub --logs, aby utworzyć 100 plików."
+fi
+
+if [[ $1 == "--help" ]]; then
+  echo "Dostępne opcje:"
+  echo "-a, --opcja-a    opis opcji a"
+  echo "-b, --opcja-b    opis opcji b"
+  echo "-c, --opcja-c    opis opcji c"
+  echo "-h, --help       wyświetla pomoc"
+else
+  echo "Nieprawidłowe użycie. Aby wyświetlić dostępne opcje, użyj parametru --help"
 fi
