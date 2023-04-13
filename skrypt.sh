f551daaf35db554b8bd90931c22855d435da9d18
Jakub Knapik <knapikj.priv@gmail.com>
2023-04-13 12:29:31 +0200

    Skrypt z dodana flaga --logs

--- skrypt.sh
+++ skrypt.sh
@@ -6,3 +6,15 @@ if [ "$1" = "--date" ]; then
 else
   echo "Użycie: $0 --date"
 fi
+
+if [[ $1 == "--logs" ]]; then
+  for i in {1..100}
+  do
+    filename="log${i}.txt"
+    echo "Nazwa pliku: ${filename}" > ${filename}
+    echo "Nazwa skryptu: skrypt.sh" >> ${filename}
+    echo "Data: $(date)" >> ${filename}
+  done
+else
+  echo "Nieprawidłowa flaga. Użyj --logs, aby utworzyć pliki logów."
+fi
